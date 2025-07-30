50820ea3cdf93cebdc757f15ccb8b8554b67e561
sumasnt jadhav <sumantjadhav1998@gmail.com>
2025-07-30 11:23:19 +0530

    PR2-CI-push to main branch for CI

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_storage_account" "PR2-CI" {
+depends_on = [ var.resource_group_name ]
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.storage_account_location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
+
+
+
+
+  
